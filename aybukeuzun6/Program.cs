d2ad61bce7136d139dbbfe89e22d906f0d855cc9
aybukeuzun <a.uzun200900@hotmail.com>
2023-05-04 18:49:24 +0300

    week6

---
+++ aybukeuzun6/Program.cs
@@ -0,0 +1,27 @@
+﻿string userName = "Aybuke";
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}.");
+
+
+//a-counter
+
+int counter = 0;
+
+for  (int i = 0; i < userName.Length; i++)
+{
+    if(userName[i] == 'a')
+    {
+        counter++; //counter = counter +1 
+    }
+}
+
+Console.WriteLine($"Your first name has {counter} a-letters.");
+
+
+//replace 
+// a - 4 
+
+string updateUsername = userName.Replace('a', '4').Replace('u', '6');
+
+Console.WriteLine(updateUsername.Length);   
